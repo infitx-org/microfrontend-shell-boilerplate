5eeb4606432838fa749f1ccb159049d92ee5868b
Ivan Annovazzi <ivan.annovazzi@gmail.com>
2021-08-24 15:07:17 +0200

    Use callback strategy for loading components from same container

--- src/App/Menu/index.tsx
+++ src/App/Menu/index.tsx
@@ -1,21 +1,31 @@
 import React from 'react';
 import { Menu } from 'components';
 import { useHistory, useLocation } from 'react-router-dom';
+import { MenuOne } from 'App/Microfrontends';
 
 function MainMenu() {
   const history = useHistory();
   const location = useLocation();
+  const menuProps = {
+    path: '/',
+    pathname: location.pathname,
+    onChange: history.push,
+  };
 
-  return (
-    <div className="layout__side-menu">
-      <Menu path="/" pathname={location.pathname} onChange={history.push}>
-        <Menu.Section label="Apps">
-          <Menu.Item path="/child" label="Child App 1 microfrontend" partial />
-          <Menu.Item path="/other" label="Child App 2 microfrontend" partial />
-        </Menu.Section>
-      </Menu>
-    </div>
+  let menu = (
+    <Menu {...menuProps}>
+      <Menu.Section label="Apps">
+        <Menu.Item path="/child" label="Child App 1 microfrontend" />
+        <Menu.Item path="/other" label="Child App 2 microfrontend" />
+      </Menu.Section>
+    </Menu>
   );
+
+  if (location.pathname.startsWith('/child')) {
+    menu = <MenuOne path="/child" pathname={location.pathname} onChange={history.push} />;
+  }
+
+  return <div className="layout__side-menu">{menu}</div>;
 }
 
 export default MainMenu;
