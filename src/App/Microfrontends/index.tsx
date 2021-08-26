74a8776eb1156bf435b5e8355313abd508f3eba5
Ivan Annovazzi <ivan.annovazzi@gmail.com>
2021-08-26 17:18:43 +0200

    Test environment variables

--- src/App/Microfrontends/index.tsx
+++ src/App/Microfrontends/index.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import { AuthConfig } from 'Config';
-import Loader from '../../utils/loader';
+import Loader from 'utils/loader';
+
+const [url, name, component] = (process.env.MICROFRONTEND_ONE || '').split('|');
 
 interface SharedProps {
   token: string | null;
   authConfig: AuthConfig;
+  [x: string]: any;
 }
 
 export function MicrofrontendOne(props: SharedProps) {
-  return <Loader url="http://localhost:3002/app.js" appName="app" component="App" {...props} />;
+  return <Loader url={url} appName={name} component={component} {...props} />;
 }
 
 export function MenuOne(props: any) {
-  return <Loader url="http://localhost:3002/app.js" appName="app" component="Menu" {...props} />;
+  return <Loader url="http://localhost:3012/app.js" appName="app" component="Menu" {...props} />;
 }
 
 export function MicrofrontendTwo(props: SharedProps) {
